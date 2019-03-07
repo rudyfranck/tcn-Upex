fe99b00e4a68fa0da19f70597ef953619d311a87
Rudy Franck <zepequenoledemysocle@gmail.com>
2019-03-07 12:09:12 +0300

    my project setup completed successfully

---
+++ TCNUpex-b/src/main/java/tcn/TCNUpex_b/App.java
@@ -0,0 +1,13 @@
+package tcn.TCNUpex_b;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
